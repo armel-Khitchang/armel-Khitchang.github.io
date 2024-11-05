616d20b9d6de75b14ac20f9e701bf6cf958ba6df
armelKhitchang <e2295867@cmaisonneuve.qc.ca>
2024-11-05 01:48:49 -0500

    Code source

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const Home = () => { 
+    return (
+        <div className='home'>
+            <Navigation/>
+            <div className='homeContent'>
+                <div className='content'>
+                    <h1>Armel Khitchang Tchoufa</h1>
+                    <h2>Analyste programmeur</h2>
+                    <div className='pdf'>
+                        <a href="./media/Cv_Armel_Khitchang_Tchoufa.pdf" target='_blank'>Telecharger</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
